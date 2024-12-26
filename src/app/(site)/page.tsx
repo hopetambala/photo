a966538ee9a83c02b55c40048bede674ff378cc0
Hope Tambala <hopetambala@outlook.com>
2024-12-26 13:03:01 -0800

    chore: replace footer

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -11,57 +11,25 @@ export default async function Home() {
         <ol>
           {photoEntries.map(({ title, slug }) => (
             <li key={slug?.current}>
-              <a
-                href={`/entries/${slug?.current}`}
-              >
-                View {title}
-              </a>
+              <a href={`/entries/${slug?.current}`}>View {title}</a>
             </li>
           ))}
         </ol>
       </main>
       <footer className={styles.footer}>
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://hopetambala.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
             src="/globe.svg"
-            alt="Globe icon"
+            alt="File icon"
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          2024 Hope Tambala
         </a>
       </footer>
     </div>
