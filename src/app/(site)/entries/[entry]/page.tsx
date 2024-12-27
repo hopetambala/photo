98690bf554cd8fa8d1a68848646d4759bfbf49e5
Hope Tambala <hopetambala@outlook.com>
2024-12-26 18:34:06 -0800

    chore: cleanup

--- src/app/(site)/entries/[entry]/page.tsx
+++ src/app/(site)/entries/[entry]/page.tsx
@@ -1,16 +1,10 @@
 import { getPhotoEntry } from "@/sanity/queries/entries";
 import { PortableText } from "@portabletext/react";
-// import type { RecipeType } from "@/types/sanity.custom-types";
 import type { Metadata } from "next";
 // import { generateRandomFallbackImage } from "@/utils/testing-helpers";
-// import { AdBanner, AdSlot } from "@/overcooked-design-system/ad-components";
 import styles from "./page.module.css";
 import Image from "next/image";
 import { OCResponsiveImage } from "@/app/overcooked-design-system/components";
-// import OcImageComponent from "@/overcooked-design-system/ui-components/image/OcImageComponent";
-// import { OCButton } from "@/overcooked-design-system/ui-components";
-// import { imgDim } from "@/utils/general";
-// import { FavoriteCard } from "@/overcooked-design-system/cooking-components";
 
 export const imgDim = (scale = 1, height = 1140, width = 760) => {
   return {
@@ -47,12 +41,7 @@ export default async function PhotoEntry(props: Params) {
   const photoEntry: any = await getPhotoEntry(params.entry);
 
   const { title, mainImage } = photoEntry;
-
-  // const clxName = [
-  //   styles["recipe-page__content"],
-  //   "class-comp-section-content-spacing-desktop",
-  // ].join(" ");
-
+  
   return (
     <main>
       <article>
