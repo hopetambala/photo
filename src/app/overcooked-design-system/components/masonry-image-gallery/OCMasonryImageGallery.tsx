79f87bff3c11df00fb19516d557b7bd2a6d1956b
Hope Tambala <hopetambala@outlook.com>
2025-11-09 16:23:45 -0800

    update with new svg

--- src/app/overcooked-design-system/components/masonry-image-gallery/OCMasonryImageGallery.tsx
+++ src/app/overcooked-design-system/components/masonry-image-gallery/OCMasonryImageGallery.tsx
@@ -84,7 +84,21 @@ const OCMasonryImageGallery = ({
             onClick={goToPrevious}
             style={{ left: "12px" }}
           >
-            ⬅️
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M15 18L9 12L15 6"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
           </button>
         )}
 
@@ -107,7 +121,21 @@ const OCMasonryImageGallery = ({
             onClick={goToNext}
             style={{ right: "12px" }}
           >
-            ➡️
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9 18L15 12L9 6"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
           </button>
         )}
       </OCDialog>
